201bbd2264caa99db158f6892641e03e4045ce5a
= ranyu920710 <= ranyu2@126.com>
2014-12-05 21:12:40 +0800

    global graph

--- Desktop/hw3/read_nodes_id.py
+++ Desktop/hw3/read_nodes_id.py
@@ -2,19 +2,17 @@ import sys
 import networkx as nx
 import random
 
-sys.path.append('/home/peachran/Desktop/hw3/HW3')
-
 def read_graph():
 	G = nx.Graph()
-	with open('public_nodes.txt','r') as f:
+	with open('egofb_nodes.txt','r') as f:
 		f.readline()
 		for data in f:
 			G.add_node(data.strip().split()[0])
-	with open('public_edges.txt','r') as f:
-		#f.readline()
-		#f.readline()
+	with open('egofb_edges.txt','r') as f:
+		f.readline()
+		f.readline()
 		for data in f:
-			G.add_edge(data.strip().split(',')[0],data.strip().split(',')[1])
+			G.add_edge(data.strip().split(' ')[0],data.strip().split(' ')[1])
 	return G
 
 def search_by_id(G,node_id):
@@ -26,9 +24,17 @@ def search_by_id(G,node_id):
 		graph_array.append((i,nx.degree(G,i)))
 		f.write(i + ' ' + str(nx.degree(G,i))+'\n')
 	f.close()
-def main():
-	G = read_graph()
-	search_by_id(G,'2014')
 
+class Gene_Graph():
+	__instance = None
+	@classmethod
+	def get_graph(cls):
+		if cls.__instance == None:
+			cls.__instance = read_graph()
+		return cls.__instance
+
+def main():
+	G = Gene_Graph.get_graph()
+	search_by_id(G,'1')
 if __name__ == '__main__':
 	main()
